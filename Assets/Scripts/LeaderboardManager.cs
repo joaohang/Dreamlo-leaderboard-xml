3fecc8ff5fcd7a396f29f4957e472b4a6054220c
João Hang <joao.hang@essentia.local>
2018-09-03 15:50:12 -0300

    Adicionando o projeto no github

---
+++ Assets/Scripts/LeaderboardManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Linq;
+using Boo.Lang;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardManager : MonoBehaviour
+{
+
+    public string url = "http://dreamlo.com/lb/";
+    public string myPrivateCode;
+    public int numberLeaders;
+
+    public Text[] leaderText;
+    public InputField username;
+    public InputField score;
+    public Button refreshScore;
+    public Button addScore;
+
+    private void Start()
+    {
+        ButtonActions();
+        DownloadAndPopulateLeaderboard();
+    }
+    
+    private void ButtonActions()
+    {
+        refreshScore.onClick.AddListener(DownloadAndPopulateLeaderboard);
+        addScore.onClick.AddListener(AddNewScore);
+    }
+
+    private void DownloadAndPopulateLeaderboard()
+    {
+        XmlDocument doc1 = new XmlDocument();
+        doc1.Load(url + myPrivateCode + "/xml/" + numberLeaders.ToString());
+        XmlElement root = doc1.DocumentElement;
+        XmlNodeList nodes = root.SelectNodes("/dreamlo/leaderboard/entry");
+
+        for (int i = 0; i < numberLeaders; i++)
+        {
+            XmlNode node = nodes[i];
+            string name = "Name: " + node["name"].InnerText;
+            string score = "- Score: " + node["score"].InnerText;
+
+            leaderText[i].text = name + score;
+        }
+    }
+
+    private void AddNewScore()
+    {
+        string username = this.username.text;
+        string score = this.score.text;
+        WWW www = new WWW(url + myPrivateCode + "/add/" + username + "/" + score);
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Dreamlo-leaderboard-xml
-Utilização do sistema Dreamlo para criação de rankings, neste projeto é utilizado xml para fazer o download dos dados do seu quadro de líderes e apresentados em textos da Unity.
+Utilização do sistema Dreamlo.com para criação de rankings, neste projeto é utilizado xml para fazer o download dos dados do seu quadro de líderes e apresentados em textos na UI e também adicionar novos líderes e seus recordes no placar.
+
+Visite o site do Dreamlo.com para saber mais.
+
+Veja também o tutorial do Sebastian Lague no link ao lado: https://www.youtube.com/watch?v=KZuqEyxYZCc
+
+Utilize o projeto como você desejar.
